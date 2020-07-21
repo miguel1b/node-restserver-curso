f68decc723003a38908f593f8414d9546ad1f180
miguelbc <miguel.bace16@gmail.com>
2020-07-21 18:14:52 -0500

    primer commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+/**
+ * PUERTO
+ */
+
+process.env.port = process.env.port || 3000;
---
+++ server/server.js
@@ -0,0 +1,48 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', (req, res) => {
+    res.json('get Usuario');
+});
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El campo nombre es requerido"
+        })
+
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+});
+
+app.put('/usuario/:id', (req, res) => {
+    const {
+        params: { id }
+    } = req;
+
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete Usuario');
+});
+
+app.listen(process.env.port, () => {
+    console.log("Server escuchando en el puerto 3000");
+})
